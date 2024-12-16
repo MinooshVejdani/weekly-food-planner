c9c73ff51519f805640414253778dd495e342f78
Mehrnoush Vejdani <mehrnoush.ghaffarzadeh@gmail.com>
2024-12-16 09:44:46 -0800

    start creating classes

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 const foodInput = document.getElementById('foodInput');
 const addButton = document.getElementById('addButton');
 const foodList = document.getElementById('foodList');
+const clearListButton = document.getElementById('clearListButton');
 
 let foods = [];
 
 window.addEventListener('load', loadFromLocalStorage);
 addButton.addEventListener('click', getFood);
+clearListButton.addEventListener('click', clearList);
 
 function loadFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('foodItems'));
+  foods = JSON.parse(localStorage.getItem('foodItems'));
+  display();
 }
 
 function saveToLocalStorage() {
   localStorage.setItem('foodItems', JSON.stringify(foods));
+}
+
+class Food {
+  constructor(name, ingredients) {
+    this.id = ++Food.idCounter;
+    this.name = name;
+    this.ingredients = ingredients;
+  }
+  
+  editIngredients(){
+    const inputIngredient = document.createElement('input');
+    input.type = text;
+    input.value = ingredient;
+    return input;
+  }
+
+  createEditButton(ingredient) {
+    const button = document.createElement('button');
+    button.innerHTML = `Edit ${ingredient}`;
+    button.addEventListener("click", () => {
+      this.editIngredients(ingredient);
+    });
+  }
+  //render all ingredients as editable input field 
+
+
+  //create a "done" button to save the edit
+
+ 
 }
 
 function getFood() {
@@ -25,28 +57,40 @@ function getFood() {
   } else {
     alert("please type your food!");
   }
-  update();
+  display();
+}
+
+function clearList() {
+  foods = [];
+  saveToLocalStorage();
+  display();
 }
 
 function removeFood() {
-
+  for(let i = 0; i < foods.length; i++) {
+    if()
+  }
   //call saveToLocalFood
   saveToLocalStorage();
+  display();
 }
 
 function editFood() {
 
 //call saveToLocalFood
 saveToLocalStorage();
+display();
 }
 
 function addIngredients() {
 
   //call saveToLocalFood
   saveToLocalStorage();
+  display();
 }
 
 function display() {
+  foodList.innerHTML = '';
   for(let i = 0; i < foods.length; i++) {
     const foodItem = document.createElement('li');
     foodItem.textContent = foods[i];
@@ -54,9 +98,3 @@ function display() {
   }
 }
 
-function update() {
-  //remove previous
-  document.querySelector('ul').innerHTML = '';
-  //call display()
-  display();
-}
